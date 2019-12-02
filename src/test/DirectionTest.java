4053c94fa1d8cb9ba3976cadd8852b6d733954e2
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-12-02 01:03:21 +0100

    Add tests for Direction.

---
+++ src/test/DirectionTest.java
@@ -0,0 +1,15 @@
+import agh.cs.evolution.Direction;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DirectionTest {
+  @Test
+  public void composeReturnsDirectionRotatedAccordingToTheGivenOne() {
+    assertEquals(Direction.EAST, Direction.NORTH.compose(Direction.EAST));
+    assertEquals(Direction.SOUTHEAST, Direction.EAST.compose(Direction.NORTHEAST));
+    assertEquals(Direction.NORTHEAST, Direction.SOUTH.compose(Direction.SOUTHWEST));
+    assertEquals(Direction.WEST, Direction.SOUTHEAST.compose(Direction.SOUTHEAST));
+    assertEquals(Direction.SOUTHEAST, Direction.WEST.compose(Direction.SOUTHWEST));
+  }
+}
