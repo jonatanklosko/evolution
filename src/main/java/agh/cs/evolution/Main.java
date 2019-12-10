6a475699479912896ca5012808cdddd213e2d78c
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-12-10 16:46:33 +0100

    Start GUI in separate thread.

--- src/main/java/agh/cs/evolution/Main.java
+++ src/main/java/agh/cs/evolution/Main.java
@@ -3,12 +3,16 @@ package agh.cs.evolution;
 import agh.cs.evolution.gui.AppGui;
 import agh.cs.evolution.simulation.SimulationParams;
 
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
     try {
       String parametersPath = args.length > 0 ? args[0] : "parameters.json";
       SimulationParams simulationParams = SimulationParams.fromFile(parametersPath);
-      new AppGui(simulationParams);
+      SwingUtilities.invokeLater(() -> {
+        new AppGui(simulationParams);
+      });
     } catch (Exception error) {
       System.out.println("Failed to start the simulation: " + error.getMessage());
     }
