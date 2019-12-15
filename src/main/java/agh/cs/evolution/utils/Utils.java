8e8a0c64fa2fc0cc2dfacb6f55686ba0131fefd9
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-12-15 02:28:26 +0100

    Set app name in pom.xml and fix maven warnings.

--- src/main/java/agh/cs/evolution/utils/Utils.java
+++ src/main/java/agh/cs/evolution/utils/Utils.java
@@ -3,7 +3,7 @@ package agh.cs.evolution.utils;
 import java.util.stream.Stream;
 
 public class Utils {
-  public static <T> Stream<T> filterType(Stream stream, Class<T> elementClass) {
+  public static <T, S> Stream<T> filterType(Stream<S> stream, Class<T> elementClass) {
     return stream
         .filter(elementClass::isInstance)
         .map(elementClass::cast);
